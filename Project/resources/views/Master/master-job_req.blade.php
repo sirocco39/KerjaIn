c65b377dd607e720c6e4c40dc10d95af4a2c3f47
olivio88 <agustinus.olivio.leoartha@gmail.com>
2025-06-02 20:19:20 +0700

    Add Navbar for Job Requester & Update Navbar + Blade Folder

--- Project/resources/views/Master/master-job_req.blade.php
+++ Project/resources/views/Master/master-job_req.blade.php
@@ -15,9 +15,9 @@
 <body>
     {{-- Navbar Section --}}
     <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
+        <div class="container-fluid pembatas">
             <a class="navbar-brand" href="/">
-                <img src="./Image/Logo/Logo Kerjain - LightBackground.png" alt="Logo Kerjain" id="logoNavbar">
+                <img src="../Image/Logo/Logo Kerjain - LightBackground.png" alt="Logo Kerjain" id="logoNavbar">
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapseFull" aria-controls="navbarCollapseFull" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,10 +26,10 @@
 
             <div class="collapse navbar-collapse" id="navbarCollapseFull">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navbarCollapse">
-                    <li class="nav-item"><a class="nav-link {{ request()->is('beranda') ? 'active' : '' }}" href="/beranda" aria-current="true">Beranda</a></li>
-                    <li class="nav-item"><a class="nav-link {{ request()->is('cari-kerja') ? 'active' : '' }}" href="/cari-kerja">Cari Kerja</a></li>
-                    <li class="nav-item"><a class="nav-link {{ request()->is('pesan') ? 'active' : '' }}" href="/pesan">Pesan</a></li>
-                    <li class="nav-item"><a class="nav-link {{ request()->is('riwayat') ? 'active' : '' }}" href="/riwayat">Riwayat</a></li>
+                    <li class="nav-item"><a class="nav-link {{ request()->is('job-req/beranda') ? 'active' : '' }}" href="/job-req/beranda" aria-current="true">Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link {{ request()->is('job-req/tawarkan-kerja') ? 'active' : '' }}" href="/job-req/tawarkan-kerja">Tawarkan Kerja</a></li>
+                    <li class="nav-item"><a class="nav-link {{ request()->is('job-req/pesan') ? 'active' : '' }}" href="/job-req/pesan">Pesan</a></li>
+                    <li class="nav-item"><a class="nav-link {{ request()->is('job-req/riwayat') ? 'active' : '' }}" href="/job-req/riwayat">Riwayat</a></li>
                 </ul>
 
                 <hr class="d-xl-none my-2">
@@ -38,29 +38,29 @@
                     <!-- Dropdown Bahasa -->
                     <li class="nav-item dropdown" id="dropLang">
                         <a class="nav-link" id="dropdownLang" data-bs-toggle="dropdown" role="button">
-                            <img src="./Image/Flag/flag-id.png" alt="Bahasa" id="langFlag">
+                            <img src="../Image/Flag/flag-id.png" alt="Bahasa" id="langFlag">
                             <span>Bahasa</span>
                             <i class="bi bi-chevron-down"></i>
                         </a>
 
                         <ul class="dropdown-menu" aria-labelledby="dropdownLang">
-                            <li><a class="dropdown-item d-flex align-items-center" href="#"><img src="./Image/Flag/flag-id.png" alt="Indonesia's Flag" class="flag"> Bahasa</a></li>
-                            <li><a class="dropdown-item d-flex align-items-center" href="#"><img src="./Image/Flag/flag-uk.png" alt="England's Flag" class="flag"> English</a></li>
+                            <li><a class="dropdown-item d-flex align-items-center" href="#"><img src="../Image/Flag/flag-id.png" alt="Indonesia's Flag" class="flag"> Bahasa</a></li>
+                            <li><a class="dropdown-item d-flex align-items-center" href="#"><img src="../Image/Flag/flag-uk.png" alt="England's Flag" class="flag"> English</a></li>
                         </ul>
                     </li>
 
                     <!-- Dropdown Profile -->
                     <li class="nav-item dropdown" id="dropProfile">
                         <a class="nav-link" id="dropdownProfile" data-bs-toggle="dropdown" role="button">
-                            <img src="./Image/Icon/user-circle.svg" alt="Profil" id="profileIcon">
+                            <img src="../Image/Icon/user-circle.svg" alt="Profil" id="profileIcon">
                             <span class="d-lg-none">Profil</span>
                         </a>
 
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownProfile">
-                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="./Image/Icon/icon-login.svg" alt="Icon Login" class="navIcon">Masuk</a></li>
-                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="./Image/Icon/icon-logout.svg" alt="Icon Login" class="navIcon">Keluar</a></li>
-                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="./Image/Icon/icon-join.svg" alt="Icon Login" class="navIcon">Menjadi Mitra</a></li>
-                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="./Image/Icon/icon-change-role.svg" alt="Icon Login" class="navIcon">Ganti Peran</a></li>
+                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="../Image/Icon/icon-login.svg" alt="Icon Login" class="navIcon">Masuk</a></li>
+                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="../Image/Icon/icon-logout.svg" alt="Icon Login" class="navIcon">Keluar</a></li>
+                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="#"><img src="../Image/Icon/icon-join.svg" alt="Icon Login" class="navIcon">Menjadi Mitra</a></li>
+                            <li><a class="dropdown-item d-flex align-items-center gap-1" href="/job_taker"><img src="../Image/Icon/icon-change-role.svg" alt="Icon Login" class="navIcon">Ganti Peran</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -72,12 +72,12 @@
     {{-- End Navbar Section --}}
 
 
-
     {{-- Main Section --}}
-    @yield('content')
+    <div class="container-fluid pembatas">
+        @yield('content')
+    </div>
 
     {{-- End Main Section --}}
-
 
 
     {{-- Footer --}}
--- Project/resources/views/dummypesan.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('master')
-
-@section('content')
-    Ini Dummy Pesan
-@endsection
-
-
