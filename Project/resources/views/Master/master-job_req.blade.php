4a7f795183d2aee64ac51c36a9b7d1b23399c30b
olivio88 <agustinus.olivio.leoartha@gmail.com>
2025-06-02 20:25:15 +0700

    Update x-icon on Job Requester's Navbar

--- Project/resources/views/Master/master-job_req.blade.php
+++ Project/resources/views/Master/master-job_req.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KerjaIn</title>
-    <link rel="icon" href="./Image/Icon/Icon Kerjain.png" type="image/x-icon">
+    <link rel="icon" href="../Image/Icon/Icon Kerjain.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
 </head>
