1846ae6fcef83a02f535ec23e2d1daae366a3125
Hansen Gunawan <hansengunawan64@gmail.com>
2025-06-08 16:13:12 +0700

    Functional Login Register

--- Project/database/migrations/2025_05_25_161400_create_users_table.php
+++ Project/database/migrations/2025_05_25_161400_create_users_table.php
@@ -6,23 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-public function up()
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->char('phone_number', 12);
+            $table->char('phone_number', 12)->nullable();
             $table->enum('role', ['user', 'admin']);
             $table->decimal('saldokerjain', 12, 2)->default(0);
             $table->boolean('is_worker')->default(false);
             $table->float('rating')->default(0);
             $table->integer('job_done')->default(0);
             $table->char('bank_acc_num', 10)->nullable();
+            $table->string('google_id')->nullable()->unique();
             $table->timestamps();
             $table->softDeletes();
+            $table->rememberToken()->nullable();
             $table->boolean('is_blocked')->default(false);
         });
     }
--- Project/routes/web.php
+++ Project/routes/web.php
@@ -1,46 +1,62 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\SocialController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::get('/', function(){
+Route::post('/send-otp', [RegisteredUserController::class, 'sendOtp'])->name('send.otp');
+
+Route::get('/auth-google-redirect', [SocialController::class, 'google_redirect'])->name('auth-google-redirect');
+Route::get('/auth-google-callback', [SocialController::class, 'google_callback'])->name('auth-google-callback');
+
+Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('register', [RegisteredUserController::class, 'store']);
+
+Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+Route::get('/', function () {
     return view('Job_Requester.dummy-job_req-landingpage');
 });
 
-Route::get('/job-req/beranda', function(){
+Route::get('/job-req/beranda', function () {
     return view('Job_Requester.dummy-job_req-beranda');
 });
 
-Route::get('/job-req/tawarkan-kerja', function(){
+Route::get('/job-req/tawarkan-kerja', function () {
     return view('Job_Requester.Tawarkankerja');
 });
 
-Route::get('/job-req/pesan', function(){
+Route::get('/job-req/pesan', function () {
     return view('Job_Requester.dummy-job_req-pesan');
 });
 
-Route::get('/job-req/riwayat', function(){
+Route::get('/job-req/riwayat', function () {
     return view('Job_Requester.dummy-job_req-riwayat');
 });
 
 
 
-Route::get('/job_taker', function(){
+Route::get('/job_taker', function () {
     return view('Job_Taker.dummy-job_taker-landingpage');
 });
 
-Route::get('/job-taker/beranda', function(){
+Route::get('/job-taker/beranda', function () {
     return view('Job_Taker.dummy-job_taker-beranda');
 });
 
-Route::get('/job-taker/cari-kerja', function(){
+Route::get('/job-taker/cari-kerja', function () {
     return view('Job_Taker.dummy-job_taker-carikerja');
 });
 
-Route::get('/job-taker/pesan', function(){
+Route::get('/job-taker/pesan', function () {
     return view('Job_Taker.dummy-job_taker-pesan');
 });
 
-Route::get('/job-taker/riwayat', function(){
+Route::get('/job-taker/riwayat', function () {
     return view('Job_Taker.dummy-job_taker-riwayat');
 });
 
@@ -50,11 +66,10 @@ Route::get('/navbar-job_taker', function () {
     return view('Master.master-job_taker');
 });
 
-Route::get('/navbar-job_req', function(){
+Route::get('/navbar-job_req', function () {
     return view('Master.master-job_req');
 });
 
 Route::get('/', function () {
     return view('landing');
 });
-
