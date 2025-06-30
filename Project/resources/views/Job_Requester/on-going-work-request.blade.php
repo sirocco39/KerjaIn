703f641674be00e0dfcf6d7777af34a452bb0412
Jennifer846578 <jtan89313@gmail.com>
2025-06-30 13:38:40 +0700

    sementara

--- Project/resources/views/Job_Requester/on-going-work-request.blade.php
+++ Project/resources/views/Job_Requester/on-going-work-request.blade.php
@@ -1,7 +1,14 @@
 @extends('Master.master-job_req')
 
 @section('content')
+    <!-- Tandai selesai -->
     <div class="container-fluid pembatas-x pembatas-y">
-        ini Contoh
+        <button type="button" class="btn" style="background-color : #a7a7a7; color: #ffffff">Tandai Selesai</button>
     </div>
+
+    <!-- label -->
+    <div class="container-fluid pembatas-x pembatas-y">
+        <button type="button" class="btn" style="background-color : #a7a7a7; color: #ffffff">Tandai Selesai</button>
+    </div>
+
 @endsection
