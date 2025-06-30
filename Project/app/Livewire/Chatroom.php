db438498283dde7d49f030a84e1d309ce9706aaf
sirocco39 <christianiwan39@gmail.com>
2025-06-30 08:18:46 +0700

    getting started

---
+++ Project/app/Livewire/Chatroom.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Chatroom extends Component
+{
+    public function render()
+    {
+        return view('livewire.chatroom');
+    }
+    
+}
