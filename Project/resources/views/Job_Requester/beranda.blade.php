e04105450536715a8c23055a073e288d5e0bc8a5
sirocco39 <christianiwan39@gmail.com>
2025-07-08 10:56:02 +0700

    fix: job requester latest request error fix

--- Project/resources/views/Job_Requester/beranda.blade.php
+++ Project/resources/views/Job_Requester/beranda.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid pembatas-x pembatas-y d-flex flex-column gap-3" id="greetings-section">
         @auth
-            <h1 class="fw-bold mb-0">Halo, {{auth()->user()->first_name}}  {{auth()->user()->last_name}}</h1>
+            <h1 class="fw-bold mb-0">Halo, {{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</h1>
         @else
             <h1 class="fw-bold mb-0">Halo, Nama Pengguna</h1>
         @endauth
@@ -20,17 +20,17 @@
         <h3 class="fw-bold mb-0">Tawaran Pekerjaan Saya Baru-baru Ini</h3>
         <div class="d-flex">
             <div class="col-12 col-xl-8 d-flex flex-column gap-4 beranda-req-kiri">
-                @if($fiveLatestRequests->isEmpty())
+                @if ($fiveLatestRequests->isEmpty())
                     <p>Anda belum pernah menawarkan pekerjaan!</p>
                 @else
                     @foreach ($fiveLatestRequests as $r)
                         <div class="work-request p-4 d-flex flex-column">
                             <?php
-                            $startdatetime  = strtotime($r->start_time);
-                            $enddatetime  = strtotime($r->end_time);
-                            $startdate   = date('d M Y', $startdatetime);
+                            $startdatetime = strtotime($r->start_time);
+                            $enddatetime = strtotime($r->end_time);
+                            $startdate = date('d M Y', $startdatetime);
                             $starttime = date('H.i', $startdatetime);
-                            $enddate  = date('d M Y', $enddatetime);
+                            $enddate = date('d M Y', $enddatetime);
                             $endtime = date('H.i', $enddatetime);
                             ?>
 
@@ -62,41 +62,40 @@
                                     <div class="icon-wrapper-beranda align-items-center align-items-md-start">
                                         <img src="{{ asset('Image/Icon/icon-dollar.svg') }}" alt="Icon Money">
                                     </div>
-                                    <span>Rp{{ number_format( $r->price, 2, ',', '.') }}</span>
+                                    <span>Rp{{ number_format($r->price, 2, ',', '.') }}</span>
                                 </li>
                             </ul>
 
                             <div class="details-bottom-segment d-flex justify-content-between mt-2">
-                                @if($r->status == "open")
-                                <div class="status">
-                                    <p class="mb-0">Menunggu Mitra</p>
-                                </div>
-                                @else
-                                    @foreach ($r->transactions as $t )
-                                        @if ($t->status == 'accepted')
-                                            <div class="status">
-                                                <p class="mb-0">Diterima</p>
-                                            </div>
-                                        @elseif($t->status == 'in progress')
-                                            <div class="status">
-                                                <p class="mb-0">Dikerjain</p>
-                                            </div>
-                                        @elseif($t->status == 'submitted')
-                                            <div class="status">
-                                                <p class="mb-0">Ditinjau</p>
-                                            </div>
-                                        @elseif($t->status == 'completed')
-                                            <div class="status" style="background-color:#E8FA0D; color: #294287;">
-                                                <p class="mb-0">Selesai</p>
-                                            </div>
-                                        @elseif($t->status == 'cancelled')
-                                            <div class="status" style="background-color: #B02A37">
-                                                <p class="mb-0">Dibatalin</p>
-                                            </div>
-                                        @endif
-                                    @endforeach
+                                @if ($r->status == 'open')
+                                    <div class="status">
+                                        <p class="mb-0">Menunggu Mitra</p>
+                                    </div>
+                                @elseif ($r->transaction->status == 'accepted')
+                                    <div class="status">
+                                        <p class="mb-0">Diterima</p>
+                                    </div>
+                                @elseif($r->transaction->status == 'in progress')
+                                    <div class="status">
+                                        <p class="mb-0">Dikerjain</p>
+                                    </div>
+                                @elseif($r->transaction->status == 'submitted')
+                                    <div class="status">
+                                        <p class="mb-0">Ditinjau</p>
+                                    </div>
+                                @elseif($r->transaction->status == 'completed')
+                                    <div class="status" style="background-color:#E8FA0D; color: #294287;">
+                                        <p class="mb-0">Selesai</p>
+                                    </div>
+                                @elseif($r->transaction->status == 'cancelled')
+                                    <div class="status" style="background-color: #B02A37">
+                                        <p class="mb-0">Dibatalin</p>
+                                    </div>
                                 @endif
-                                <a class="detail-req-button" data-bs-toggle="modal" data-bs-target="#detailModal" data-slug="{{ $r->slug }}" data-edit-url="{{ route('requesttt.edit', $r->slug) }}" data-delete-url="{{ route('requesttt.destroy', $r->slug) }}">DETAIL</a>
+                                <a class="detail-req-button" data-bs-toggle="modal" data-bs-target="#detailModal"
+                                    data-slug="{{ $r->slug }}"
+                                    data-edit-url="{{ route('requesttt.edit', $r->slug) }}"
+                                    data-delete-url="{{ route('requesttt.destroy', $r->slug) }}">DETAIL</a>
                             </div>
                         </div>
                         {{-- <div class="dropdown text-end">
@@ -119,7 +118,8 @@
             </div>
 
             <div class="col beranda-req-kanan d-xl-flex align-items-center justify-content-end">
-                <img class="d-lg-flex" src="{{ asset('Image/orang/ilus-beranda.svg') }}" alt="People Give Money" id="ilus-beranda">
+                <img class="d-lg-flex" src="{{ asset('Image/orang/ilus-beranda.svg') }}" alt="People Give Money"
+                    id="ilus-beranda">
             </div>
         </div>
     </div>
@@ -129,7 +129,8 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header d-flex justify-content-between align-items-center">
-                    <button type="button" class="btn-close" id="modal-close-button" aria-label="Close" data-bs-dismiss="modal"></button>
+                    <button type="button" class="btn-close" id="modal-close-button" aria-label="Close"
+                        data-bs-dismiss="modal"></button>
                 </div>
                 <div id="modal-content-container" class="p-3">
                     <h1 class="fw-bold mb-3" id="modal-detail-title">Nama Lowongan Kerja</h1>
@@ -180,23 +181,26 @@
         </div>
     </div>
 
-    <div class="modal fade" id="deleteConfirmation" tabindex="-1" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
+    <div class="modal fade" id="deleteConfirmation" tabindex="-1" aria-labelledby="deleteConfirmationModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header d-flex justify-content-between align-items-center">
-                    <img src="{{asset('Image/Icon/icon-danger.svg')}}" alt="Danger Icon">
+                    <img src="{{ asset('Image/Icon/icon-danger.svg') }}" alt="Danger Icon">
                     <h1 class="fw-bold mb-0" style="color: #B02A37">Hapus Tawaran</h1>
-                    <img src="{{asset('Image/Icon/icon-danger.svg')}}" alt="Danger Icon">
+                    <img src="{{ asset('Image/Icon/icon-danger.svg') }}" alt="Danger Icon">
                 </div>
                 <div id="modal-content-container" class="p-3">
                     <p class="mb-3 text-center fw-medium fs-5">Apakah kamu yakin menghapus tawaran?</p>
                 </div>
                 <div class="detail-buttons-placeholder d-flex gap-3 justify-content-center mt-auto">
-                    <a class="details-button-item btn-tawar-modal text-decoration-none" data-bs-target="#detailModal" data-bs-toggle="modal" id="kembali-button-section">Tidak</a>
+                    <a class="details-button-item btn-tawar-modal text-decoration-none" data-bs-target="#detailModal"
+                        data-bs-toggle="modal" id="kembali-button-section">Tidak</a>
                     <form id="delete-request-form" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="details-button-item btn-hapus-modal text-decoration-none">Ya</button>
+                        <button type="submit"
+                            class="details-button-item btn-hapus-modal text-decoration-none">Ya</button>
                     </form>
                 </div>
             </div>
@@ -223,7 +227,7 @@
             const deleteForm = document.getElementById('delete-request-form');
 
 
-            modal.addEventListener('show.bs.modal', function (event) {
+            modal.addEventListener('show.bs.modal', function(event) {
                 // Tombol yang memicu modal
                 const button = event.relatedTarget;
                 const slug = button.getAttribute('data-slug');
@@ -256,33 +260,50 @@
 
                         modalTitle.textContent = data.title || '-';
                         modalLocation.textContent = data.location || '-';
-                        modalDate.textContent = startDatetime.toLocaleDateString('id-ID', { day: '2-digit', month: 'short', year: 'numeric' });
-                        modalTime.textContent = `${startDatetime.toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })} - ${endDatetime.toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })}`;
-                        modalPrice.textContent = parseFloat(data.price || 0).toLocaleString('id-ID', { minimumFractionDigits: 2 });
+                        modalDate.textContent = startDatetime.toLocaleDateString('id-ID', {
+                            day: '2-digit',
+                            month: 'short',
+                            year: 'numeric'
+                        });
+                        modalTime.textContent =
+                            `${startDatetime.toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })} - ${endDatetime.toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })}`;
+                        modalPrice.textContent = parseFloat(data.price || 0).toLocaleString('id-ID', {
+                            minimumFractionDigits: 2
+                        });
                         modalDescription.textContent = data.description || '-';
 
                         // Tentukan status berdasarkan data
                         if (data.status === 'open') {
                             modalStatus.innerHTML = `<p class="mb-0">Menunggu Mitra</p>`;
-                            buttonAction1.innerHTML = `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-1" href="${editUrl}">Sunting</a>`;
-                            buttonAction2.innerHTML = `<a class="details-button-item btn-hapus-modal text-decoration-none" data-bs-target="#deleteConfirmation" data-bs-toggle="modal" id="button-action-2">Hapus</a>`
+                            buttonAction1.innerHTML =
+                                `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-1" href="${editUrl}">Sunting</a>`;
+                            buttonAction2.innerHTML =
+                                `<a class="details-button-item btn-hapus-modal text-decoration-none" data-bs-target="#deleteConfirmation" data-bs-toggle="modal" id="button-action-2">Hapus</a>`
                             deleteForm.setAttribute('action', deleteUrl);
                         } else if (data.status === 'closed') {
                             const latestTransaction = data.transactions?.slice(-1)[0];
                             const statusText = getStatusText(latestTransaction?.status);
 
-                            if(statusText === 'Diterima'){
-                                buttonAction1.innerHTML = `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-1" href="#">Pesan</a>`
-                                buttonAction2.innerHTML = `<a class="details-button-item btn-hapus-modal text-decoration-none" id="button-action-2" href="#">Batalin</a>`
-                            } else if(statusText === 'Dikerjain'){
-                                buttonAction1.innerHTML = `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-1" href="#">Pesan</a>`
-                                buttonAction2.innerHTML = `<a class="details-button-item btn-hapus-modal text-decoration-none" id="button-action-2" href="#">Laporin</a>`
-                            } else if(statusText === 'Ditinjau'){
-                                buttonAction1.innerHTML = `<a class="details-button-item btn-terima-modal text-decoration-none" id="button-action-1" href="#">Konfirmasi</a>`
-                                buttonAction2.innerHTML = `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-2" href="#">Pesan</a>`
-                                buttonAction3.innerHTML = `<a class="details-button-item btn-hapus-modal text-decoration-none" id="button-action-3" href="#">Laporin</a>`
-                            } else if(statusText === 'Selesai'){
-                                buttonAction1.innerHTML = `<a class="details-button-item btn-terima-modal text-decoration-none" id="button-action-1" href="#">Ulas</a>`
+                            if (statusText === 'Diterima') {
+                                buttonAction1.innerHTML =
+                                    `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-1" href="#">Pesan</a>`
+                                buttonAction2.innerHTML =
+                                    `<a class="details-button-item btn-hapus-modal text-decoration-none" id="button-action-2" href="#">Batalin</a>`
+                            } else if (statusText === 'Dikerjain') {
+                                buttonAction1.innerHTML =
+                                    `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-1" href="#">Pesan</a>`
+                                buttonAction2.innerHTML =
+                                    `<a class="details-button-item btn-hapus-modal text-decoration-none" id="button-action-2" href="#">Laporin</a>`
+                            } else if (statusText === 'Ditinjau') {
+                                buttonAction1.innerHTML =
+                                    `<a class="details-button-item btn-terima-modal text-decoration-none" id="button-action-1" href="#">Konfirmasi</a>`
+                                buttonAction2.innerHTML =
+                                    `<a class="details-button-item btn-tawar-modal text-decoration-none" id="button-action-2" href="#">Pesan</a>`
+                                buttonAction3.innerHTML =
+                                    `<a class="details-button-item btn-hapus-modal text-decoration-none" id="button-action-3" href="#">Laporin</a>`
+                            } else if (statusText === 'Selesai') {
+                                buttonAction1.innerHTML =
+                                    `<a class="details-button-item btn-terima-modal text-decoration-none" id="button-action-1" href="#">Ulas</a>`
                             }
 
                             modalStatus.innerHTML = `<p class="mb-0">${statusText}</p>`;
@@ -308,7 +329,7 @@
             });
 
             // Event listener untuk menutup modal
-            modal.addEventListener('hidden.bs.modal', function () {
+            modal.addEventListener('hidden.bs.modal', function() {
                 // Hapus kelas 'choosed' dari semua elemen 'work-request'
                 const workRequestCards = document.querySelectorAll('.work-request');
                 workRequestCards.forEach(card => {
@@ -318,12 +339,18 @@
 
             function getStatusText(status) {
                 switch (status?.toLowerCase()) {
-                    case 'accepted': return 'Diterima';
-                    case 'in progress': return 'Dikerjain';
-                    case 'submitted': return 'Ditinjau';
-                    case 'completed': return 'Selesai';
-                    case 'cancelled': return 'Dibatalin';
-                    default: return '-';
+                    case 'accepted':
+                        return 'Diterima';
+                    case 'in progress':
+                        return 'Dikerjain';
+                    case 'submitted':
+                        return 'Ditinjau';
+                    case 'completed':
+                        return 'Selesai';
+                    case 'cancelled':
+                        return 'Dibatalin';
+                    default:
+                        return '-';
                 }
             }
 
@@ -351,8 +378,5 @@
             });
 
         });
-
-
     </script>
 @endsection
-
